e30e15cb55049ad0b8ef827a1e421917b07119a1
SofiaBejar <150276443+SofiaBejar@users.noreply.github.com>
2025-03-12 09:13:57 +0100

    Update beforeinstall.sh

--- deploy-script/beforeinstall.sh
+++ deploy-script/beforeinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-cd /opt/codedeploy-agent/deployment-root/420095b2-6a59-4a36-ab9b-b146f739c57d/d-4C2I0KPMB/deployment-archive
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 chmod +x gradlew
 ./gradlew build
 mv build/libs/studentify-1.0.0.war studentify.war
