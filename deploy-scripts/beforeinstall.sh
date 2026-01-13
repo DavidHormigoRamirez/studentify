e49ded6c26b27930a2462736df224c054a632194
David Hormigo Ramírez <dhorram948@g.educaand.es>
2026-01-13 10:10:59 +0100

    Remove Workflows

--- deploy-scripts/beforeinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-chmod +x gradlew
-./gradlew build
-mv build/libs/studentify-1.0.0.war studentify.war
